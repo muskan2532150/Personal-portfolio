f46f6ca4ec32f8c1dc4960b3ed55b7bf82bf725f
muskan2532150 <rg2532150@gmail.com>
2023-07-08 01:59:35 +0530

    Add validation to user Model file

--- portfolio_api/app/models/user.rb
+++ portfolio_api/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates: :name, presence: true , length:{maximum: 50}
+    validates: :github_link, presence: true
+    validates: :linkedln_link, presence: true 
+    validates: :email, presence: true 
+
 end
