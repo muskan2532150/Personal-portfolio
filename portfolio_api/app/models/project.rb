28a281315fcd5fd51494ed9196f9871661a83eea
muskan2532150 <rg2532150@gmail.com>
2023-07-08 01:59:35 +0530

    Add validation to  Model file

--- portfolio_api/app/models/project.rb
+++ portfolio_api/app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+    validates: :name, presence: true , length:{maximum: 50}
+    validates: :des, presence: true
+    validates: :img, presence: true
+    validates: :source_code, presence: true 
+    validates: :live_version, presence: true 
 end
