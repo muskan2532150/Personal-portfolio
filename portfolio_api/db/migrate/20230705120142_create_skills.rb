7a414a0fffdbf13b35df8b4442d2ecb3406c3a9e
muskan2532150 <rg2532150@gmail.com>
2023-07-21 01:40:35 +0530

    Update skill migration

--- portfolio_api/db/migrate/20230705120142_create_skills.rb
+++ portfolio_api/db/migrate/20230705120142_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration[7.0]
   def change
     create_table :skills do |t|
       t.string :name
-      t.string :img
+      t.string :icon
 
       t.timestamps
     end
